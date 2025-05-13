c8e0a02cc7eb524804c99705e845e96aad68c24e
Chris K <72310639+DragonGrenade@users.noreply.github.com>
2025-05-13 06:19:29 +0300

    first commit

---
+++ Portfolio_piece_3.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CINEMA THEATER</title>
+
+    <link rel="stylesheet" href="css\images_styles.css">
+    <link rel="stylesheet" href="css\contact_styles.css">
+
+    <script src="javascript/email_script.js" async></script>
+
+
+</head>
+<body>
+<a href="/Portfolio%20Website" class="back-button">
+    <img src="images\Arrow 1.svg" alt="Back to Home arrow" class="arrow-icon">
+  </a>
+<section class="images_section">
+    <div class="flex-box_container">
+      <div class="image-container">
+        <img src="images/Image_1.png" alt="Image 1">
+      </div>
+      <div class="image-container">
+        <img src="images/Image_2.png" alt="Image 2">
+      </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<section class="brief">
+  <div class="flex-box_container">
+  <div class="brief-left">
+  <div class="info-row">
+    <span class="label">Category:</span>
+    <span class="value">Web Design + Web Development</span>
+  </div>
+  <div class="info-row">
+    <span class="label">Client:</span>
+    <span class="value">NONE - SAMPLE WORK</span>
+  </div>
+  <div class="info-row">
+    <span class="label">Duration:</span>
+    <span class="value">8 WEEKS</span>
+  </div>
+
+
+  <a href="../Cinema%20Theater%20Website/index.php" class="preview-button">
+  PREVIEW WEBSITE
+  <img src="images/arrow_button.svg" alt="Arrow Icon" class="arrow-icon">
+</a>
+
+</div>
+    <div class="brief-right">
+      <p>
+      The Cinedaily website project is designed to reduce clutter, prioritize relevant content, and help users quickly accomplish key tasks—whether that’s booking a movie, pre-ordering concessions, or getting in touch.
+      </p>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+<section class="challenges-section">
+  <h2 class="main-heading">What are the challenges for the cinema theater?</h2>
+  <div class="flex-box_container_challenges">
+    <!-- First Column -->
+    <div class="challenges-left">
+      <div class="challenge">
+      <h3>Poor Online Booking Experience</h3>
+        <p>Users are redirected to an irrelevant third-party platform that feels disconnected from the theater brand.</p>
+        <p>Some users cannot book tickets due to technical issues or complex flows they face with the linked booking platform.</p>
+      </div>
+      <div class="challenge">
+      <h3>High Bounce Rate</h3>
+        <p>Visitors leave within seconds due to slow loading, poor layout and insufficient content.</p>
+       </div>
+      <div class="challenge">
+        <h3>Loss of Trust & Brand Disconnect</h3>
+        <p>The third-party platform design doesn’t reflect the cinema's tone, story, or branding—users feel lost or unsure.</p>
+        <p>Mismatched fonts, inconsistent colors, or poor-quality images degrade perceived professionalism.</p>
+      </div>
+      <div class="challenge">
+        <h3>No Involvement or Community Feeling</h3>
+        <p>No voting systems or polls for upcoming movies—missing an opportunity for audience engagement.</p>
+        <p>No behind-the-scenes content, cast interviews, or special local promotions.</p>
+      </div>
+      <div class="challenge">
+        <h3>No Showcasing of Facilities or Tech</h3>
+        <p>No mention of auditoriums, seating comfort, sound systems (e.g., Dolby Atmos), screen quality, or special rooms (VIP, kids, etc.).</p>
+        <p>Also, people do not understand how the technology will affect their experience. Missed opportunity to create desire or set expectations visually.</p>
+      </div>
+    </div>
+
+    <!-- Second Column -->
+    <div class="challenges-right">
+      <div class="challenge">
+        <h3>Negative Testimonials & Reviews</h3>
+        <p>As a result of people avoiding to use online booking due to distrust or unfamiliarity, there is frustration due to long waiting lines in person that could’ve been avoided with smoother online booking since more people would book online.</p>
+        <p>Users sometimes blame the theater directly even if the fault lies with the third-party service for errors and inaccessibility around the booking platform.</p>
+      </div>
+      <div class="challenge">
+        <h3>Lack of Real-Time Information</h3>
+        <p>No clear, live updates on available seats, upcoming movies, or sold-out sessions.</p>
+        <p>No notification system for booking confirmations, cancellations, or changes.</p>
+      </div>
+      <div class="challenge">
+        <h3>Lack of Accessibility & Inclusivity</h3>
+        <p>No accessible design (for screen readers, visually impaired, etc.).</p>
+        <p>No clear info about subtitled movies, audio-described showings, or wheelchair-accessible seating.</p>
+      </div>
+      <div class="challenge">
+        <h3>Poor “About Us” Page</h3>
+        <p>No communication of the mission or their evolution.</p>
+        <p>The cinema has a story and origins but lacks skills to communicate it to consumers. They have no compelling story.</p>
+        <p>Feels way too generic and users have no emotional reason to care or remember the theater.</p>
+        <p>Consumers don’t understand what sets them apart (e.g., indie-friendly, community-run, high-end experience).</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+<div class="image-container">
+        <img src="images\Sitemap.png" alt="Image 2">
+      </div>
+
+
+
+
+<?php include("includes/contact_include.php"); ?>
+
+
+</body>
+</html>
---
+++ includes/portfolio_pieces_include.php
@@ -0,0 +1,51 @@
+<section class="portfolio_section">
+
+  <h1>WEB DEVELOPMENT</h1>
+
+  <div class="flex_container_portfolio">
+
+    <div class="portfolio_item">
+      <a href="Portfolio_piece_1.php">
+        <img src="images/Piece_1.png" alt="Project 1" />
+        <div class="portfolio_footer">
+          <span>#WEBFLOW EDITOR</span>
+          <span>#DOCTOR | PULMONOLOGIST</span>
+        </div>
+      </a>
+    </div>
+
+    <div class="portfolio_item">
+      <a href="">
+        <img src="images/Piece_2.png" alt="Project 2" />
+        <div class="portfolio_footer">
+          <span>#WEBFLOW EDITOR</span>
+          <span>#THERAPY</span>
+        </div>
+      </a>
+    </div>
+
+    <div class="portfolio_item">
+      <a href="Portfolio_piece_3.php">
+        <img src="images/Piece_3.png" alt="Project 3" />
+        <div class="portfolio_footer">
+          <span>#CUSTOM CODE</span>
+          <span>#CINEMA THEATER WEBSITE</span>
+        </div>
+      </a>
+    </div>
+
+  </div>
+
+  <div class="flex_container_portfolio single_row">
+    <div class="portfolio_item full_width_item">
+      <a href="">
+        <img src="images/Piece_4.png" alt="Project 4" />
+        <div class="portfolio_footer">
+        <span>#WEBFLOW EDITOR</span>
+        <span>#PHOTOGRAPHY</span>
+        </div>
+      </a>
+    </div>
+  </div>
+
+</section>
